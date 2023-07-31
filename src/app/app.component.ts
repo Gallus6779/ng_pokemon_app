d0b88c3899b968c920a94d80dfc9b539156c4678
Gallus6779 <gallus.noah@yahoo.com>
2023-07-31 12:19:54 +0100

    exercice - liste de pokemon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,8 +4,8 @@ import { Pokemon } from './pokemon';
 
 @Component({
   selector: 'app-root',
-  templateUrl : './app.component.html'
-  // templateUrl : './app.pokemonlist.html'
+  // template : `Bonjour`,
+  templateUrl : './app.pokemonlist.html'
 })
 export class AppComponent implements OnInit{
   pokemonList: Pokemon[] = POKEMONS;
