2d02c7d2304de47d15066a0ba68632f0ed1b649b
Gallus6779 <gallus.noah@yahoo.com>
2023-12-22 18:05:27 +0100

    Nouvelle version de l'apprentissage de Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
@@ -7,12 +7,25 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   template: `
-    <h1>Welcome to {{title}}!</h1>
+    <h1>Welcome to {{pokemonList[1]}}!</h1>
 
     <router-outlet></router-outlet>
   `,
   styles: [],
 })
-export class AppComponent {
-  title = 'ng-pokemon-app';
+export class AppComponent implements OnInit{
+  pokemonList = [
+    'Salameche',
+    'Bulbizarre',
+    'Carapuce'
+  ];
+
+  ngOnInit() {
+    console.table(this.pokemonList);
+    this.selectPokemon('bulbizarre');
+  }
+
+  selectPokemon(pokemonName: string){
+    console.log(`Vous avez cliquer sur le pokemon ${pokemonName}`);
+  }
 }
